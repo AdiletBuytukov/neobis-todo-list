d5061d33e3c7cf6f8c34bcd02784db0f4f269f6c
AdiletBuytukov <abuytukov@gmail.com>
2024-05-09 13:36:11 +0600

    Реализованы:  1. Интрефейс проекта (нефинальный вариант).  2. Строка приветсвия, где можно поменять имя пользователя.  3. Функционал добавления задачи.

--- todo.js
+++ todo.js
@@ -1,3 +1,13 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
 
+function addTask() {
+  if (inputBox.value === '') {
+    alert("Поле не может быть пустым, введите текст");
+  } else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+  }
 
-alert('Я JavaScript')
+}
